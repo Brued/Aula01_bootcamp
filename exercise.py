097b8090f14307e2ba2d8c48b266e1053ff95fb3
Brued <Bruedbes@gmail.com>
2024-06-29 22:50:51 -0300

    exercicio2

---
+++ exercise.py
@@ -0,0 +1,8 @@
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+x = input ("Digite um numero:")
+y = input ("Digite o segundo numero:")
+
+sum = int (x) + int (y)
+
+print ("A soma é: ", sum)
